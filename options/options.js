ea01c15fef7d92c1181270d2ce113b1755685777
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-11-17 15:12:25 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "paneuro-44456",
   url: "https://paneuro-44456.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
