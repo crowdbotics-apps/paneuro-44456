70ed3bb1a815ff3799f94f47259186761c006450
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-11-17 16:01:47 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI311/index.js
+++ screens/ScreenAI311/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View, Image, Button } from 'react-native';
 
-const LogoutScreen = () => {
+const SuccessScreen = () => {
   return <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Image style={styles.image} source={{
         uri: 'https://tinyurl.com/42evm3m3'
       }} />
-        <Text style={styles.text}>Are you sure you want to Log Out</Text>
-        <View style={styles.buttonContainer}>
-          <Button title="Yes" onPress={() => {}} />
-          <Button title="Cancel" onPress={() => {}} />
-        </View>
+        <Text style={styles.text}>Successfully removed/deleted</Text>
+        <Button title="Close" onPress={() => {}} />
       </View>
     </SafeAreaView>;
 };
@@ -19,29 +16,24 @@ const LogoutScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5F5F5',
     alignItems: 'center',
     justifyContent: 'center'
   },
   content: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    padding: 20
   },
   image: {
-    width: 100,
-    height: 100,
+    width: 200,
+    height: 200,
     marginBottom: 20
   },
   text: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '60%'
+    marginBottom: 20,
+    color: '#333'
   }
 });
-export default LogoutScreen;
+export default SuccessScreen;
